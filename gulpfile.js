15ca555830fc6d98a9ec6cc2e16331216e6c366f
kristall-93 <kristushapaf@yandex.ru>
2020-09-10 23:05:25 +0300

    deleted excess files and code

--- gulpfile.js
+++ gulpfile.js
@@ -21,10 +21,10 @@ gulp.task('sass', function(){
 gulp.task('style', function(){
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
-        'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
-        'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css',
-        'node_modules/rateyo/src/jquery.rateyo.css'      
+        // 'node_modules/slick-carousel/slick/slick.css',
+        // 'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
+        // 'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css',
+        // 'node_modules/rateyo/src/jquery.rateyo.css'      
     ])
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
@@ -32,14 +32,14 @@ gulp.task('style', function(){
 });
 
 gulp.task('script', function(){
-    return gulp.src([
-        'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
-        'node_modules/rateyo/src/jquery.rateyo.js'          
-    ])
-    .pipe(concat('libs.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('app/js'))
+    // return gulp.src([
+    //     'node_modules/slick-carousel/slick/slick.js',
+    //     'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+    //     'node_modules/rateyo/src/jquery.rateyo.js'          
+    // ])
+    // .pipe(concat('libs.min.js'))
+    // .pipe(uglify())
+    // .pipe(gulp.dest('app/js'))
 });
 
 gulp.task('html', function(){
